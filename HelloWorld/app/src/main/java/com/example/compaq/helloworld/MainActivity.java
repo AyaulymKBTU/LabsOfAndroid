669ed71c20d89e79ab56636b2f8c80c2e69c6e8b
Mukhametkaliyeva <pryanikh@gmail.com>
2017-09-07 16:36:34 +0600

    first two labs

---
+++ HelloWorld/app/src/main/java/com/example/compaq/helloworld/MainActivity.java
@@ -0,0 +1,23 @@
+package com.example.compaq.helloworld;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Toast;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+
+    public void onBnClickMe(View v){
+
+        Toast myToast = Toast.makeText(getApplicationContext(),
+                "Hello, World!",
+                Toast.LENGTH_LONG);
+        myToast.show();
+    }
+}
